3c095bf361ba052282cf7f5982fd92a65295cf28
jacowp357 <jacowp357@gmail.com>
2017-04-16 16:48:12 +0200

    refactor

--- src/tutorials/pgm_example.py
+++ src/tutorials/pgm_example.py
@@ -21,6 +21,9 @@ from pgmpy.factors.discrete import TabularCPD
 import matplotlib.mlab as mlab
 from pgmpy.estimators import ConstraintBasedEstimator
 import matplotlib.pyplot as plt
+from itertools import cycle
+from scipy import interp
+np.set_printoptions(suppress=True)
 
 ###################
 # Data processing #
@@ -45,6 +48,7 @@ Attribute Information:
 
 df = pd.read_csv('data.csv', sep=',', engine='python', header='infer')
 
+# mapping from labels to ordinals #
 class_map = {'unacc': 0, 'acc': 1, 'good': 2, 'vgood': 3}
 buying_map = {'low': 0, 'med': 1, 'high': 2, 'vhigh': 3}
 maint_map = {'low': 0, 'med': 1, 'high': 2, 'vhigh': 3}
@@ -53,22 +57,22 @@ persons_map = {'2': 0, '4': 1, 'more': 2}
 lug_boot_map = {'small': 0, 'med': 1, 'big': 2}
 safety_map = {'low': 0, 'med': 1, 'high': 2}
 
-df = df.replace({"class": class_map})
-df = df.replace({"buying": buying_map})
-df = df.replace({"maint": maint_map})
-df = df.replace({"doors": doors_map})
-df = df.replace({"persons": persons_map})
-df = df.replace({"lug_boot": lug_boot_map})
-df = df.replace({"safety": safety_map})
+df = df.replace({"class": class_map,
+                 "buying": buying_map,
+                 "maint": maint_map,
+                 "doors": doors_map,
+                 "persons": persons_map,
+                 "lug_boot": lug_boot_map,
+                 "safety": safety_map})
 
-df_train, df_test = train_test_split(df, test_size=0.2, random_state=1)
+df_train, df_test = train_test_split(df, test_size=0.25, random_state=1)
 
 ######################
 # Structure learning #
 ######################
 
 # c = ConstraintBasedEstimator(df)
-# model = c.estimate(significance_level=0.01)
+# model = c.estimate(significance_level=0.05)
 
 #######################
 # Graph visualisation #
@@ -86,117 +90,93 @@ df_train, df_test = train_test_split(df, test_size=0.2, random_state=1)
 #     g.edge(item[0], item[1])
 # g.render(filename='pgm')
 
-###########################
-# Construct Bayes network #
-###########################
+########################################################################
+# Construct Naive Bayes network (without help from structure learning) #
+########################################################################
 
-model1 = BayesianModel([('class', 'doors'),
-                        ('class', 'safety'),
-                        ('class', 'maint'),
-                        ('class', 'buying'),
-                        ('class', 'persons'),
-                        ('class', 'lug_boot')])
+model = BayesianModel([('class', 'doors'),
+                       ('class', 'safety'),
+                       ('class', 'maint'),
+                       ('class', 'buying'),
+                       ('class', 'persons'),
+                       ('class', 'lug_boot')])
 
-model2 = BayesianModel([('safety', 'class'),
-                        ('maint', 'class'),
-                        ('persons', 'class'),
-                        ('buying', 'class')])
-
-model1.fit(df_train, estimator_type=MaximumLikelihoodEstimator)
-model2.fit(df_train, estimator_type=MaximumLikelihoodEstimator)
+model.fit(df_train, estimator=MaximumLikelihoodEstimator)
 
 # print(model.get_cpds('class').values)
 # print(model.active_trail_nodes('doors', observed='class'))
 
 # can also use Belief propagation #
-inference1 = VariableElimination(model1)
-inference2 = VariableElimination(model2)
+inference = BeliefPropagation(model)
 
-# phi_query = inference.query(variables=['class'])
-# print(phi_query['class'])
-# phi_query = inference.query(variables=['class'], evidence={'doors': 0})
-# print(phi_query['class'])
+print("Class variable prior:\n{}\n".format(inference.query(variables=['class'])['class']))
+print("Class variable posterior after certain observation:\n{}\n".format(inference.query(variables=['class'], evidence={'doors': 0, 'persons': 2})['class']))
 
-#####################
-# Predict test data #
-#####################
+# #####################
+# # Predict test data #
+# #####################
 
-predict_data = df_test.copy()
-predict_data.drop(['class'], axis=1, inplace=True)
-# y_pred = model.predict(predict_data)
+# predict_data = df_test.copy()
+# predict_data.drop(['class'], axis=1, inplace=True)
+# # y_pred = model.predict(predict_data)
 
-pred_values = []
-for index, data_point in predict_data.iterrows():
-    prob_dist = inference1.query(
-        variables=['class'], evidence=data_point.to_dict())['class'].values
-    pred_values.append(prob_dist)
+# pred_values = []
+# for index, data_point in predict_data.iterrows():
+#     prob_dist = inference.query(variables=['class'], evidence=data_point.to_dict())['class'].values
+#     pred_values.append(prob_dist)
 
-print('Classification accuracy: %0.3f' % accuracy_score(
-    df_test['class'].values, [np.argmax(i) for i in pred_values]))
-print(confusion_matrix(df_test['class'].values,
-                       [np.argmax(i) for i in pred_values]))
-print(f1_score(df_test['class'].values, [np.argmax(i)
-                                         for i in pred_values], average='micro'))
+# ####################
+# # Model evaluation #
+# ####################
 
-predict_data.drop(['doors', 'lug_boot'], axis=1, inplace=True)
-# y_pred = model.predict(predict_data)
+# print("Classification accuracy: {0:.3f}%".format(100 * accuracy_score(df_test['class'].values, [np.argmax(i) for i in pred_values])))
+# print("F1 score: {0:.3f}\n".format(f1_score(df_test['class'].values, [np.argmax(i) for i in pred_values], average='weighted')))
+# print("Confusion matrix:\n{}\n".format(confusion_matrix(df_test['class'].values, [np.argmax(i) for i in pred_values])))
 
-pred_values = []
-for index, data_point in predict_data.iterrows():
-    prob_dist = inference2.query(
-        variables=['class'], evidence=data_point.to_dict())['class'].values
-    pred_values.append(prob_dist)
+# y_test = label_binarize(df_test['class'].values, classes=[0, 1, 2, 3])
+# pred_values = np.array(pred_values)
 
-print('Classification accuracy: %0.3f' % accuracy_score(
-    df_test['class'].values, [np.argmax(i) for i in pred_values]))
-print(confusion_matrix(df_test['class'].values,
-                       [np.argmax(i) for i in pred_values]))
-print(f1_score(df_test['class'].values, [np.argmax(i)
-                                         for i in pred_values], average='micro'))
+# fpr = dict()
+# tpr = dict()
+# roc_auc = dict()
+# n_classes = 4
+# for i in range(n_classes):
+#     fpr[i], tpr[i], _ = roc_curve(y_test[:, i], pred_values[:, i])
+#     roc_auc[i] = auc(fpr[i], tpr[i])
 
+# # Compute micro-average (flattened) ROC curve and ROC area #
+# fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), pred_values.ravel())
+# roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
 
-# # y_test = label_binarize(df_test['class'].values, classes=[0, 1, 2, 3])
-# # pred_values = np.array(pred_values)
+# plt.figure(0)
+# plt.plot(fpr["micro"], tpr["micro"], label="micro-average ROC curve (area = {0:0.3f})".format(roc_auc["micro"]))
 
-# # fpr = dict()
-# # tpr = dict()
-# # roc_auc = dict()
-# # for i in range(4):
-# #     fpr[i], tpr[i], _ = roc_curve(y_test[:, i], pred_values[:, i])
-# #     roc_auc[i] = auc(fpr[i], tpr[i])
+# # for i in range(n_classes):
+# #     plt.plot(fpr[i], tpr[i], label="ROC curve of class {0} (area = {1:0.2f})".format(i, roc_auc[i]))
 
-# # # Compute micro-average ROC curve and ROC area
-# # fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), pred_values.ravel())
-# # roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+# plt.plot([0, 1], [0, 1], 'k--')
+# plt.xlim([0.0, 1.0])
+# plt.ylim([0.0, 1.05])
+# plt.xlabel('False Positive Rate')
+# plt.ylabel('True Positive Rate')
+# plt.title('ROC curve')
+# plt.legend(loc="lower right")
 
-# # for i in range(4):
-# #     plt.plot(fpr[i], tpr[i], label='ROC curve %d: (area = %0.2f)' % (i, roc_auc[i]))
-# # plt.plot([0, 1], [0, 1], color='navy', linestyle='--')
-# # plt.xlim([0.0, 1.0])
-# # plt.ylim([0.0, 1.05])
-# # plt.xlabel('False Positive Rate')
-# # plt.ylabel('True Positive Rate')
-# # plt.title('Receiver operating characteristic example')
-# # plt.legend(loc="lower right")
-# # plt.show()
+######################################################
+# Incorporate uncertainty/noise to a random variable #
+######################################################
 
-
-# print(model.edges())
-
-#################################
-# Incorporate uncertainty/noise #
-#################################
-
-# convert to Markov network #
+# convert to Markov network (undirected graph) #
 model = model.to_markov_model()
 
 # can not be a factor graph since not all random variables are connected to factors #
 # model = model.to_factor_graph()
 # print(model.check_model())
 
-# add the new received variable and connect#
+# add the new received variable and connect #
 model.add_node('doors_received')
-model.add_edge('doors', 'doors_received')
+model.add_node('persons_received')
+model.add_edges_from([('persons', 'persons_received'), ('doors', 'doors_received')])
 
 # binary symmetric channel noise #
 # n = 4
@@ -208,27 +188,30 @@ model.add_edge('doors', 'doors_received')
 #         else:
 #             cpd.append(0)
 
-# Gaussian noise #
-std = 0.2
-states = [mlab.normpdf(2, 2, std), mlab.normpdf(2, 3, std), mlab.normpdf(2, 4, std), mlab.normpdf(2, 5, std),
-          mlab.normpdf(2, 3, std), mlab.normpdf(3, 3, std), mlab.normpdf(
-              4, 3, std), mlab.normpdf(5, 3, std),
-          mlab.normpdf(2, 4, std), mlab.normpdf(3, 4, std), mlab.normpdf(
-              4, 4, std), mlab.normpdf(5, 4, std),
-          mlab.normpdf(2, 5, std), mlab.normpdf(3, 5, std), mlab.normpdf(4, 5, std), mlab.normpdf(5, 5, std)]
+# Add Gaussian noise around each state #
+std = 1
+cpd1 = [mlab.normpdf(0, 0, std), mlab.normpdf(0, 1, std), mlab.normpdf(0, 2, std), mlab.normpdf(0, 3, std),
+        mlab.normpdf(1, 0, std), mlab.normpdf(1, 1, std), mlab.normpdf(1, 2, std), mlab.normpdf(1, 3, std),
+        mlab.normpdf(2, 0, std), mlab.normpdf(2, 1, std), mlab.normpdf(2, 2, std), mlab.normpdf(2, 3, std),
+        mlab.normpdf(3, 0, std), mlab.normpdf(3, 1, std), mlab.normpdf(3, 2, std), mlab.normpdf(3, 3, std)]
 
-# p(doors_received|doors) - doors was original information, but we
-# received a noisy version of it!
-factor = DiscreteFactor(['doors', 'doors_received'],
-                        cardinality=[4, 4], values=cpd)
+cpd2 = [mlab.normpdf(0, 0, std), mlab.normpdf(0, 1, std), mlab.normpdf(0, 2, std),
+        mlab.normpdf(1, 0, std), mlab.normpdf(1, 1, std), mlab.normpdf(1, 2, std),
+        mlab.normpdf(2, 0, std), mlab.normpdf(2, 1, std), mlab.normpdf(2, 2, std)]
 
-print(factor)
-# factor.reduce([('doors_received', 0)])
-# factor.normalize()
-# print(factor)
+# p(doors_received|doors) - doors was original information, but we received a noisy version of it! #
+# not necessary to normalise cpd for Markov network #
+factor1 = DiscreteFactor(['doors', 'doors_received'], cardinality=[4, 4], values=cpd1)
+factor2 = DiscreteFactor(['persons', 'persons_received'], cardinality=[3, 3], values=cpd2)
+
+# print(factor1)
+# factor1.reduce([('doors_received', 0)])
+# # # factor.normalize()
+# print(factor1)
 
 # add the factor to the network #
-model.add_factors(factor)
+model.add_factors(factor1)
+model.add_factors(factor2)
 
 # print(model.nodes())
 # print(model.edges())
@@ -236,8 +219,5 @@ model.add_factors(factor)
 # can also use VE #
 inference = BeliefPropagation(model)
 
-phi_query = inference.query(variables=['class'])
-print(phi_query['class'])
-phi_query = inference.query(
-    variables=['class'], evidence={'doors_received': 0})
-print(phi_query['class'])
+print("Class variable prior:\n{}\n".format(inference.query(variables=['class'])['class']))
+print("Class variable posterior after noisy observation:\n{}\n".format(inference.query(variables=['class'], evidence={'doors_received': 0, 'persons_received': 2})['class']))
