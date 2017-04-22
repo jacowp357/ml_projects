15d8d264df98aa8acd6a53cdbdcb22ba6eeab8b9
jacowp357 <jacowp357@gmail.com>
2017-04-22 09:59:15 +0200

    add node pairs mutual information calc

--- src/tests/hamming.py
+++ src/tests/hamming.py
@@ -2,12 +2,13 @@
 #
 # :Author: Jaco du Toit <jacowp357@gmail.com>
 # :Date: 04/04/2017
-# :Description: This code explores a deterministic Hamming (7, 4) code example as a
-#               test for mutual information between all codeword bits.
+# :Description: This code explores a deterministic Hamming (7, 4) code example
+#               as a test for mutual information between all codeword bits.
 #
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import itertools
 plt.style.use('ggplot')
 
 
@@ -23,7 +24,6 @@ def gen_data(N):
              [1 0 1 1 0 0 1]]
     """
     data = []
-
     for i in range(N):
         m = np.random.randint(0, 2, 4)
         if ((m[0] + m[1] + m[2] - 3) % 2) == 1:
@@ -64,26 +64,28 @@ def calc_MI(X, Y, bins):
     H_Y = shan_entropy(c_Y)
     H_XY = shan_entropy(c_XY)
     MI = H_X + H_Y - H_XY
-    print("H(X) + H(Y) - H(X, Y) = {} + {} - {} = {}".format(H_X, H_Y, H_XY, MI))
     return MI
 
 
 if __name__ == '__main__':
     np.set_printoptions(linewidth=2000)
-    df = pd.DataFrame(gen_data(10000), columns=['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7'])
-
+    df = pd.DataFrame(gen_data(10000), columns=['b1',
+                                                'b2',
+                                                'b3',
+                                                'b4',
+                                                'b5',
+                                                'b6',
+                                                'b7'])
     A = df.as_matrix()
     bins = 2
     # n = 7
     # matMI = np.zeros((7, 7))
-
+    # pairwise MI #
     # for ix in np.arange(7):
     #     for jx in np.arange(ix + 1, 7):
     #         matMI[ix, jx] = calc_MI(A[:, ix], A[:, jx], bins)
-
     # print(matMI)
 
-    # # matMI = (matMI - matMI.mean()) / (matMI.std())
     # fig, ax = plt.subplots()
     # plt.pcolor(matMI, cmap=plt.cm.coolwarm, alpha=0.7)
     # ax.set_yticks(np.arange(matMI.shape[0]) + 0.5, minor=False)
@@ -98,10 +100,26 @@ if __name__ == '__main__':
 
     print("Entropy b1, b2, b3:        H(X) = {}".format(shan_entropy(np.histogramdd(np.column_stack((A[:, 0], A[:, 1], A[:, 2])), bins=bins)[0])))
     print("Entropy b5:                H(Y) = {}".format(shan_entropy(np.histogram(A[:, 4], bins=bins)[0])))
-    print("Entropy b1, b2, b3, b5: H(X, Y) = {}".format(shan_entropy(np.histogramdd(np.column_stack((A[:, 0], A[:, 1], A[:, 2], A[:, 4])), bins=bins)[0])))
-    print(calc_MI(A[:, 0].reshape(-1, 1), A[:, 4].reshape(-1, 1), bins))
-    print(calc_MI(np.column_stack((A[:, 0], A[:, 1])), A[:, 4].reshape(-1, 1), bins))
-    print(calc_MI(np.column_stack((A[:, 0], A[:, 1], A[:, 2])), A[:, 4].reshape(-1, 1), bins))
-    print(calc_MI(np.column_stack((A[:, 1], A[:, 2], A[:, 3])), A[:, 5].reshape(-1, 1), bins))
-    print(calc_MI(np.column_stack((A[:, 0], A[:, 2], A[:, 3])), A[:, 6].reshape(-1, 1), bins))
+    print("Entropy b1, b2, b3, b5: H(X, Y) = {}\n".format(shan_entropy(np.histogramdd(np.column_stack((A[:, 0], A[:, 1], A[:, 2], A[:, 4])), bins=bins)[0])))
 
+    # calc MI for all possible pairs of nodes #
+    nodes = {0: 'b1', 1: 'b2', 2: 'b3', 3: 'b4', 4: 'b5', 5: 'b6', 6: 'b7'}
+
+    n = len(nodes)
+
+    for i in range(n):
+        temp_nodes = nodes.copy()
+        # remove node of interest #
+        del temp_nodes[i]
+        # get remaining column names of interest #
+        col_names = list(temp_nodes.values())
+        m = n - 1
+        # create temp truth table to get all combinations #
+        df_temp = pd.DataFrame(list(itertools.product([False, True], repeat=m)), columns=col_names)
+        # drop first row with all false cases #
+        df_temp = df_temp.drop(df_temp.index[0])
+        # for each row in temp_df calculate the MI #
+        for j in range(len(df_temp)):
+            # get column names that are true #
+            b = list(df_temp.ix[j + 1, :][df_temp.ix[j + 1, :]].index.values)
+            print(nodes[i], b, calc_MI(df[[nodes[i]]].as_matrix(), df[b].as_matrix(), bins))
